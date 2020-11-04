f0c76c92ce8c45cb0b5d8b1ec8fdd083dbef48a3
xpsilvester <xpsilvester@gmail.com>
2020-11-04 17:03:05 +0800

    modulize

--- slider.js
+++ slider.js
@@ -1,27 +1,33 @@
-window.onload = function(){
-    const dom = new Proxy({}, {
-        get(target, property) {
-          return function(attrs = {}, ...children) {
-            const el = document.createElement(property);
-            for (let prop of Object.keys(attrs)) {
-              el.setAttribute(prop, attrs[prop]);
-            }
-            for (let child of children) {
-              if (typeof child === 'string') {
-                child = document.createTextNode(child);
-              }
-              el.appendChild(child);
-            }
-            return el;
-          }
+const dom = new Proxy({}, {
+  get(target, property) {
+    return function(attrs = {}, ...children) {
+      const el = document.createElement(property);
+      for (let prop of Object.keys(attrs)) {
+        el.setAttribute(prop, attrs[prop]);
+      }
+      for (let child of children) {
+        if (typeof child === 'string') {
+          child = document.createTextNode(child);
         }
-    });
-    
+        el.appendChild(child);
+      }
+      return el;
+    }
+  }
+});
+
+class XPSlider{
+  constructor(className){
+    this.className = className
+  }
+  init(){
+    let $el = this.append()
+    this.listen($el)
+  }
+  append(){
     let dotChild = []
-    
-    let num = document.querySelector('.slider').children[0].children.length
+    let num = document.querySelector(this.className).children[0].children.length
     let dataIndex = 'data-index'
-    
     for(let i=0;i < num;i++){
         let child = dom.div({
             class: i == 0 ? 'dot active' : 'dot',
@@ -29,19 +35,26 @@ window.onload = function(){
         })
         dotChild.push(child)
     }
-    
     let el = dom.div({class:'dots'},...dotChild)
-    
-    let $slider = document.querySelector('.slider');
-    
-    $slider.appendChild(el)
-    el.addEventListener('click',function(e){
-        if(e.target.className.match('dots')){
-            return
-        }
-        let index = parseInt(e.target.getAttribute('data-index'))
-        document.querySelector('.slider .dots .dot.active').classList.remove('active')
-        e.target.classList.add('active')
-        document.querySelector('.slider ul').style.left =  -index * 100 + '%'
+    document.querySelector(this.className).appendChild(el)
+    return el
+  }
+  listen($dom){
+    $dom.addEventListener('click',(e) => {
+      if(e.target.className.match('dots')){
+        return
+      }
+      let index = parseInt(e.target.getAttribute('data-index'))
+      document.querySelector(`${this.className} .dots .dot.active`).classList.remove('active')
+      e.target.classList.add('active')
+      document.querySelector(this.className).children[0].style.left =  -index * 100 + '%'
     })
+  }
 }
+
+window.onload = function(){
+  let slider = new XPSlider('.slider')  
+  slider.init()
+}
+
+
